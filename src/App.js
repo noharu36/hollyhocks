d87156e2b3401bbf16d9d52a07279ba2629fcbb0
haru <132886617+noharu36@users.noreply.github.com>
2023-10-25 10:42:30 +0000

    秋の味覚 ~キノコ~

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import kinoko from "./kinoko.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header bg-wed">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-wed-dark">
-          Welcome to <code>WED</code> Community.
-        </p>
-        <a
-          className="text-wed-dark underline"
-          href="https://wed.company/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn WED
-        </a>
+      <header className="App-header bg-color1">
+        <img src={kinoko} className="App-logo" alt="logo" />
+        <div className="Logos">
+          <img src={kinoko} className="App-logo2" alt="logo" />
+          <img src={kinoko} className="App-logo3" alt="logo" />
+        </div>
+        <p className="text-color2">There are three Goombas.</p>
       </header>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,8 @@ module.exports = {
     colors: {
       wed: '#D2D2C6',
       'wed-dark': '#6D695D',
+      'color1': '#713b6a',
+      'color2': '#02b512',
     },
     extend: {
     },
